3d004144bd95d22036f5ba1292464860da22b822
syachrilRamadhan <syachrilbls99@gmail.com>
2023-10-01 12:36:49 +0700

    features: (font) add font style

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,22 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import Head from "next/head";
+import { useRouter } from "next/router";
 
+const monserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-mont",
+});
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${monserrat.variable} font-mont w-full min-h-screen flex flex-col items-center justify center`}></main>
+      <Component key={router.asPath} {...pageProps} />
+    </>
+  );
 }
